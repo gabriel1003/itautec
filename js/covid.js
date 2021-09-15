c4db4053ae3598b2164a52886f62957539fcb578
Gabriel Merconi <merconigabriel22@gmail.com>
2021-09-15 14:39:50 -0300

    aumentar e diminuir fonte

--- js/covid.js
+++ js/covid.js
@@ -18,3 +18,49 @@ function validate() {
   alert( "Mensagem enviada com sucesso" );
   return( true );
 }
+
+function IncreaseHeader(){
+	//Get the tag you want to change
+	var headerTag = document.getElementById('acessibilidade');
+	
+	// Get the current font size
+	var currentFontSize = headerTag.style.fontSize;
+	
+	//Take out the px in the end
+	currentFontSize = currentFontSize.slice(0, -2);
+	
+	//Make currenFontSize an Integer
+	currentFontSize = parseInt(currentFontSize);
+	
+	//Make new font size while increase currenFontSize by 1
+	var newFontSize = currentFontSize + 1;
+	
+	//Make newFontsize a string and add px in the end
+	newFontSize = newFontSize.toString() + "px";
+	
+	//Set new style
+	headerTag.style.fontSize = newFontSize;
+}
+
+function DecreaseHeader(){
+	//Get the tag you want to change
+	var headerTag = document.getElementById('acessibilidade');
+	
+	// Get the current font size
+	var currentFontSize = headerTag.style.fontSize;
+	
+	//Take out the px in the end
+	currentFontSize = currentFontSize.slice(0, -2);
+	
+	//Make currenFontSize an Integer
+	currentFontSize = parseInt(currentFontSize);
+	
+	//Make new font size while decrease currenFontSize by 1
+	var newFontSize = currentFontSize - 1;
+	
+	//Make newFontsize a string and add px in the end
+	newFontSize = newFontSize.toString() + "px";
+	
+	//Set new style
+	headerTag.style.fontSize = newFontSize;
+}
